39eba95da70baa29540aae05bd2aeb6fb55b0560
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-25 15:11:32 +0300

    Filter Genre

--- app/handlers/menu.py
+++ app/handlers/menu.py
@@ -1,27 +1,49 @@
-from enum import Enum
-
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from service.api import ApiClient
-from service.service import card
+from service.service import card, ExtendedEnum
 
 
-class ActionMenu(Enum):
+class ActionMenu(ExtendedEnum):
     """Меню"""
     anons = 'анонсы'
     filter = 'фильтр'
     search = 'поиск'
 
-    @staticmethod
-    def list():
-        return list(map(lambda c: c.value, ActionMenu))
+
+class ActionMenuFilter(ExtendedEnum):
+    """Меню фильтр"""
+    genre = 'отфильтровать по жанры'
+    ordering = 'отсортировать'
+
+
+class ActionMenuFilterOrdering(ExtendedEnum):
+    """Меню Сортировки"""
+    rating = 'рейтинг'
+    votes = 'голоса'
 
 
 class MenuStatesGroup(StatesGroup):
     """Состояния меню"""
     waiting_for_menu = State()
+
+
+class FilterStatesGroup(StatesGroup):
+    """Состояния меню фильтра"""
+    menu_filter = State()
+
+
+class FilterGenreStatesGroup(StatesGroup):
+    """Состояния меню фильтра жанры"""
+    menu_filter_genre = State()
+
+
+class FilterOrderingStatesGroup(StatesGroup):
+    """Состояния меню фильтра сортировка"""
+    menu_filter_ordering = State()
+    menu_filter_number = State()
 
 
 async def start_menu(message: types.Message):
@@ -34,7 +56,7 @@ async def start_menu(message: types.Message):
 
 
 async def menu_chosen(message: types.Message, state: FSMContext):
-    """"""
+    """Контролер меню"""
     if message.text.lower() not in ActionMenu.list():
         await message.answer(
             "Пожалуйста, выберите пункт меню, используя клавиатуру ниже.")
@@ -45,7 +67,12 @@ async def menu_chosen(message: types.Message, state: FSMContext):
             await message.answer(ActionMenu.anons.value)
             await anons(message, state)
         case ActionMenu.filter.value:
-            pass
+            await state.finish()
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            for name in ActionMenuFilter:
+                keyboard.add(name.value)
+            await message.answer('Фильтр:', reply_markup=keyboard)
+            await FilterStatesGroup.menu_filter.set()
         case ActionMenu.search.value:
             pass
 
@@ -69,6 +96,62 @@ async def anons(message: types.Message, state: FSMContext):
         await state.finish()
 
 
+async def menu_filter_chosen(message: types.Message, state: FSMContext):
+    """Контролер меню фильтра"""
+    if message.text.lower() not in ActionMenuFilter.list():
+        await message.answer(
+            "Пожалуйста, выберите пункт меню, используя клавиатуру ниже.")
+        return
+
+    match message.text.lower():
+        case ActionMenuFilter.genre.value:
+            await state.finish()
+            genre_list = await ApiClient().get_genre()
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            for name in genre_list:
+                keyboard.add(name['title'])
+            await message.answer('Жанры:', reply_markup=keyboard)
+            await FilterGenreStatesGroup.menu_filter_genre.set()
+        case ActionMenuFilter.ordering.value:
+            await state.finish()
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            for name in ActionMenuFilterOrdering:
+                keyboard.add(name.value)
+            await message.answer('Сортировка:', reply_markup=keyboard)
+            await FilterOrderingStatesGroup.menu_filter_ordering.set()
+
+
+async def menu_filter_genre_chosen(message: types.Message, state: FSMContext):
+    """Фильтр по жанрам"""
+    genre_list = await ApiClient().get_genre()
+    if message.text.lower() not in [genre['title'] for genre in genre_list]:
+        await message.answer(
+            "Пожалуйста, выберите пункт меню, используя клавиатуру ниже.")
+        return
+
+    data = await ApiClient().get_filter_genre(message.text.lower())
+
+    await message.answer(f'Количество: {data["count"]}')
+    for item in data['results']:
+        await message.answer(
+            card(item),
+            reply_markup=types.ReplyKeyboardRemove(),
+            parse_mode=types.ParseMode.HTML
+        )
+    await state.finish()
+
+
+async def menu_filter_ordering_chosen(
+        message: types.Message,
+        state: FSMContext
+):
+    """Сортировка"""
+    if message.text.lower() not in ActionMenuFilterOrdering.list():
+        await message.answer(
+            "Пожалуйста, выберите пункт меню, используя клавиатуру ниже.")
+        return
+
+
 def register_handlers_menu(dp: Dispatcher):
     dp.register_message_handler(
         start_menu,
@@ -78,4 +161,12 @@ def register_handlers_menu(dp: Dispatcher):
     dp.register_message_handler(
         menu_chosen,
         state=MenuStatesGroup.waiting_for_menu
+    )
+    dp.register_message_handler(
+        menu_filter_chosen,
+        state=FilterStatesGroup.menu_filter
+    )
+    dp.register_message_handler(
+        menu_filter_genre_chosen,
+        state=FilterGenreStatesGroup.menu_filter_genre
     )
