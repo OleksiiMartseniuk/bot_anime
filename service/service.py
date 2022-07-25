12e0c81ff7e9a9484ec5e64c09b091d868f5d219
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-25 13:52:47 +0300

    edit catd

--- service/service.py
+++ service/service.py
@@ -10,8 +10,8 @@ def card(data: dict, schedules: bool = False) -> str:
         date_time = date_time.strftime('%H:%M')
     else:
         date_time = 'В течении дня'
-    time = f'Time: {date_time}' if schedules else ''
+    time = f'Time: {date_time} \n\n' if schedules else ''
     return f"{fmt.hide_link(data['url_image_preview'])} " \
-           f"{data['title'].split('/')[0]} \n\n" \
-           f"{time} \n\n" \
+           f"<b>{data['title'].split('/')[0]}</b> \n\n" \
+           f"{time} " \
            f"<a href='{data['link']}'>animevost</a>"
