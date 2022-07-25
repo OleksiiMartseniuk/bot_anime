c72ebce1124cf38183f85f361b9313ba7c9be87e
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-25 12:28:15 +0300

    import handlers

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import BotCommand
 
+from app.handlers.schedules import register_handlers_schedules
+from app.handlers.common import register_handlers_common
+
 from settings import TOKEN_BOT
 
 
@@ -14,6 +17,7 @@ logger = logging.getLogger(__name__)
 async def set_commands(bot: Bot):
     """Команды бота"""
     commands = [
+        BotCommand(command="/anime_schedules", description="Расписания"),
         BotCommand(command="/cancel", description="Отменить текущее действие")
     ]
     await bot.set_my_commands(commands)
@@ -29,8 +33,12 @@ async def main():
     bot = Bot(token=TOKEN_BOT)
     dp = Dispatcher(bot, storage=MemoryStorage())
 
+    register_handlers_schedules(dp)
+    register_handlers_common(dp)
+
     await set_commands(bot)
 
+    await dp.skip_updates()
     await dp.start_polling()
 
 
