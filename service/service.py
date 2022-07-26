cb32d5b9252021099560ff03fa9e1c97ba2a5944
martsenyk.l@gmail.com <6n8at3TUS7>
2022-07-26 23:37:55 +0300

    add Week

--- service/service.py
+++ service/service.py
@@ -4,22 +4,26 @@ from datetime import datetime
 import aiogram.utils.markdown as fmt
 
 
-class ExtendedEnum(Enum):
+class Week(Enum):
+    monday = 'понедельник'
+    tuesday = 'вторник'
+    wednesday = 'среда'
+    thursday = 'четверг'
+    friday = 'пятница'
+    saturday = 'суббота'
+    sunday = 'воскресенье'
 
-    @classmethod
-    def list(cls):
-        return list(map(lambda c: c.value, cls))
 
-
-def card(data: dict, schedules: bool = False) -> str:
+def card(data: dict) -> str:
     """Вывод аниме"""
     if data['timer']:
         date_time = datetime.fromtimestamp(data['timer'])
         date_time = date_time.strftime('%H:%M')
     else:
         date_time = 'В течении дня'
-    time = f'Time: {date_time} \n\n' if schedules else ''
     return f"{fmt.hide_link(data['url_image_preview'])} " \
            f"<b>{data['title'].split('/')[0]}</b> \n\n" \
-           f"{time} " \
-           f"<a href='{data['link']}'>animevost</a>"
+           f"Время выхода ~({date_time}) \n" \
+           f"Рейтинг {data['rating']}\n" \
+           f"Голоса {data['votes']}\n" \
+           f"<a href='{data['link']}'>Смотреть на animevost.org</a>"
