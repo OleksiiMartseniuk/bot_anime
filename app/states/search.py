03ee585f776ef99fd2ff876e3d71b1f8297e6c61
martsenyk.l@gmail.com <6n8at3TUS7>
2022-08-01 20:00:59 +0300

    edit SearchName add [waiting_for_page]

--- app/states/search.py
+++ app/states/search.py
@@ -4,3 +4,4 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 class SearchName(StatesGroup):
     """Состояния для поиска"""
     waiting_for_name = State()
+    waiting_for_page = State()
