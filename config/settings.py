0fc0fff867399bd25e0d320d613e2a511a92180a
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-02 11:52:46 +0300

    add settings database

--- config/settings.py
+++ config/settings.py
@@ -3,3 +3,9 @@ import os
 
 TOKEN_BOT = os.getenv('TOKEN_BOT')
 API_URL = 'http://localhost/v1/anime/'
+
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+HOST_DB = os.getenv('HOST_DB')
+PORT_DB = os.getenv('PORT_DB')
