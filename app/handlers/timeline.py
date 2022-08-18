b72e0c4eb89bea62bbf359b60cd6ca9892e36d98
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-18 20:54:31 +0300

    create timeline

---
+++ app/handlers/timeline.py
@@ -0,0 +1,45 @@
+import logging
+
+from aiogram import Dispatcher, types
+
+from database.db import DataBaseClient
+
+from service.api import ApiClient
+from service.service import card
+
+
+logger = logging.getLogger(__name__)
+
+
+async def start_timeline(message: types.Message):
+    data = await ApiClient().get_indefinite_exit()
+
+    logger.info(f'Пользователь [{message.from_user.id}] '
+                f'выбрал действия [timeline]')
+
+    if not data:
+        logger.error(f'Данные с сервера неверны запрос [get_indefinite_exit]')
+        await message.answer('Что-то пошло не так!!!')
+        return
+
+    # Запись статистики
+    await DataBaseClient().set_statistics(
+        message.from_user.id,
+        'timeline',
+        'Лента'
+    )
+
+    for item in data['results']:
+        await message.answer_photo(
+            item['url_image_preview'],
+            caption=card(item),
+            parse_mode=types.ParseMode.HTML
+        )
+
+
+def register_handlers_timeline(dp: Dispatcher):
+    dp.register_message_handler(
+        start_timeline,
+        commands="timeline",
+        state="*"
+    )
