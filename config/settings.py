90decfda1932948b78aec809b14cd91f1f512f4b
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-24 15:29:27 +0300

    delete settings database

--- config/settings.py
+++ config/settings.py
@@ -5,9 +5,3 @@ TOKEN_BOT = os.getenv('TOKEN_BOT')
 
 API_URL = 'http://localhost/api/v1/'
 API_KEY = os.getenv('API_KEY')
-
-POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-HOST_DB = os.getenv('HOST_DB')
-PORT_DB = os.getenv('PORT_DB')
