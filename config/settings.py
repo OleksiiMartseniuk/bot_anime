19c536636cb2c9d44b0e527a17cf59682dca8882
OleksiiMartseniuk <martsenyk.l@gmail.com>
2022-08-24 15:28:54 +0300

    edit path api

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ import os
 
 TOKEN_BOT = os.getenv('TOKEN_BOT')
 
-API_URL = 'http://localhost/v1/'
+API_URL = 'http://localhost/api/v1/'
 API_KEY = os.getenv('API_KEY')
 
 POSTGRES_DB = os.getenv('POSTGRES_DB')
