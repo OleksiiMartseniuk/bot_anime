ff11c996c5a750a23355ed6a3f43e1b5910bbdff
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-14 19:17:19 +0300

    add class ClientBase

--- service/api.py
+++ service/api.py
@@ -1,79 +1,53 @@
-import httpx
-import logging
-
+from base.api_client import ClientBase
 from config.settings import API_URL, API_KEY
 
-logger = logging.getLogger(__name__)
 
+class ApiClient(ClientBase):
+    """Api"""
 
-class ApiClient:
     def __init__(self):
         self.api_url = API_URL
-
-    async def _get(self, url: str, params: dict = {}, **kwargs) -> dict | None:
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(url, params=params, **kwargs)
-                if response.status_code == 200:
-                    return response.json()
-                else:
-                    logger.error(f'[{url}] - statuscode'
-                                 f'[{response.status_code}]')
-        except httpx.RequestError as exc:
-            logger.error(f'Исключения {exc.__class__} [{str(exc)}]')
-
-    async def _post(self, url: str, data: dict, **kwargs) -> dict | None:
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.post(url, data=data, **kwargs)
-                if response.status_code == 201:
-                    return response.json()
-                else:
-                    logger.error(f'[{url}] - statuscode'
-                                 f'[{response.status_code}]')
-        except httpx.RequestError as exc:
-            logger.error(f'Исключения {exc.__class__} [{str(exc)}]')
 
     async def get_anons(self) -> dict | None:
         """Получения анонсов"""
         url = self.api_url + 'anime/'
         params = {'anons': 'True'}
-        return await self._get(url, params)
+        return await self.get(url, params)
 
     async def get_anime_day(self, day: str) -> dict | None:
         """Получения аниме по дню недели"""
         url = self.api_url + 'anime/'
         params = {'day_week': day}
-        return await self._get(url, params)
+        return await self.get(url, params)
 
     async def get_genre(self) -> list | None:
         """Жанры"""
         url = self.api_url + 'anime/genre/'
-        return await self._get(url)
+        return await self.get(url)
 
     async def get_filter_genre(self, title: str, page: int = 1) -> dict | None:
         """Фильтр по жанрам"""
         url = self.api_url + 'anime/'
         params = {'genre': title, 'page': page}
-        return await self._get(url, params)
+        return await self.get(url, params)
 
     async def get_count_anime(self) -> int | None:
         """Количество аниме"""
         url = self.api_url + 'anime/'
-        result = await self._get(url)
+        result = await self.get(url)
         return result.get('count')
 
     async def search(self, q: str, page: int = 1) -> dict | None:
         """Поиск по названию"""
         url = self.api_url + 'anime/'
         params = {'search': q, 'page': page}
-        return await self._get(url, params)
+        return await self.get(url, params)
 
     async def get_indefinite_exit(self) -> dict | None:
         """Аниме с неопределенным сроком выхода"""
         url = self.api_url + 'anime/'
         params = {'indefinite_exit': True, 'ordering': '-updated'}
-        return await self._get(url, params)
+        return await self.get(url, params)
 
     async def sent_statistic(
             self,
@@ -89,7 +63,7 @@ class ApiClient:
             'action': action,
             'message': message
         }
-        return await self._post(url, data, headers=headers)
+        return await self.post(url, data, headers=headers)
 
     async def sent_message(
             self,
@@ -103,4 +77,4 @@ class ApiClient:
             'id_user': id_user,
             'message': message
         }
-        return await self._post(url, data, headers=headers)
+        return await self.post(url, data, headers=headers)
