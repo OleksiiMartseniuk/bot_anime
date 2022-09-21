0c5aa252083edcee5f91af51fff739a61d28ef8e
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-21 21:11:26 +0300

    add function get_anime_anilibria

--- service/service.py
+++ service/service.py
@@ -1,10 +1,15 @@
 import math
 import re
+import logging
 
-from typing import List
-
+from pydantic import ValidationError
 from enum import Enum
 from datetime import datetime
+
+from . import schemas
+
+
+logger = logging.getLogger(__name__)
 
 
 class Week(Enum):
@@ -39,7 +44,7 @@ def card(data: dict, schedules: bool = False) -> str:
            f"<a href='{get_link_mirror(data['link'])}'>Зеркало v2.vost.pw</a>"
 
 
-def get_page_list(page_count: int) -> List[int]:
+def get_page_list(page_count: int) -> list[int]:
     """Получения списка страниц"""
     count = page_count / 20
     return [x for x in reversed(range(1, math.ceil(count) + 1))]
@@ -57,3 +62,39 @@ def get_link_mirror(link: str) -> str | None:
 def get_image(url_image_preview: str, telegram_id_file: str | None) -> str:
     """Получения доступной картинки"""
     return telegram_id_file if telegram_id_file else url_image_preview
+
+
+def get_anime_anilibria(item: dict) -> schemas.AnimeAniLibria | None:
+    """Получения схемы AnimeAniLibria"""
+    try:
+        names = schemas.Names(
+            ru=item.get('names').get('ru'),
+            en=item.get('names').get('en')
+        )
+        posters = schemas.Posters(
+            small=item.get('posters').get('small').get('url'),
+            medium=item.get('posters').get('medium').get('url'),
+            original=item.get('posters').get('original').get('url')
+        )
+        type = schemas.PType(
+            full_string=item.get('type').get('full_string'),
+            string=item.get('type').get('string'),
+            series=item.get('type').get('series'),
+            length=item.get('type').get('length')
+        )
+        anime = schemas.AnimeAniLibria(
+            id=item.get('id'),
+            code=item.get('code'),
+            names=names,
+            posters=posters,
+            updated=item.get('updated'),
+            last_change=item.get('last_change'),
+            type=type,
+            genres=item.get('genres'),
+            description=item.get('description'),
+        )
+    except ValidationError as e:
+        logger.error(e)
+        return
+
+    return anime
