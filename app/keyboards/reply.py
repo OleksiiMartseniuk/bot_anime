c143d6c9a54b6be101f75fe88a80480bd40249ca
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-28 16:04:29 +0300

    add capitalize

--- app/keyboards/reply.py
+++ app/keyboards/reply.py
@@ -26,7 +26,7 @@ def get_schedules() -> ReplyKeyboardMarkup:
     """Расписания"""
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     for name in available_anime:
-        keyboard.add(name)
+        keyboard.add(name.capitalize())
     return keyboard
 
 
